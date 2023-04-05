f82d6c44cba16aa807711d6fb604b1808558312b
Emi <andere@uchicago.edu>
2023-04-05 11:57:19 -0500

    first files push

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from helpers import make_more_human
+
+# Accept an entire file as input
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python testing.py <filename>")
+        sys.exit(1)
+    filename = sys.argv[1]
+    with open(filename, "r") as f:
+        text: str = f.read()
+
+    max_tokens = len(text) if len(text) < 4096 else 4096
+    make_more_human(text, max_tokens=max_tokens)
+
+if __name__ == "__main__":
+    main()
